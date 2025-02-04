1fabd561e1a101efb8688975c8dde475a1207719
Dominic Camill <106774701+LikeMyGames@users.noreply.github.com>
2025-02-03 21:44:01 -0800

    updating electron stuff

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
 import { app, BrowserWindow } from 'electron'
+import { execFile } from 'node:child_process'
+
+// const child = execFile('./API/API.exe', (error, stdout, stderr) => {
+//   if (error) {
+//     throw error;
+//   }
+//   console.log(stdout);
+// })
+
+// child.on('exit', () => {
+// 	child.kill()
+// })
+
+const API = execFile('./API/API.exe', (err, stdout, stderr) => {
+	if (err) {
+		throw err
+	}
+
+	console.log(stdout)
+})
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        icon: `./favicon.ico`
+		width: 1280,
+		height: 720,
+		icon: `./favicon.ico`,
+		titleBarStyle: 'hidden',
+		titleBarOverlay: {
+			color: '#00000000',
+			symbolColor: '#92ff5c',
+			height: 40
+        }
     })
 
     win.loadFile('index.html')
 }
 
+
 app.whenReady().then(() => {
     createWindow()
 })
+
+app.setUserTasks([
+	{
+	  program: process.execPath,
+	  arguments: '--new-window',
+	  iconPath: process.execPath,
+	  iconIndex: 0,
+	  title: 'New Window',
+	  description: 'Create a new window'
+	}
+])
